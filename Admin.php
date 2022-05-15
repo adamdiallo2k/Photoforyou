692120883fc13f3c149822ba758d4ddd774278a4
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2022-05-15 15:08:06 +0200

    Add files via upload

---
+++ Admin.php
@@ -0,0 +1,11 @@
+<?php include "inc/header.php"; ?>
+<div class="container">
+    <div class="jumbotron">
+      <h1 class="display-4">Bonjour</h1>
+      <hr class="my-4">
+      <p>Vous etes connecter Mr l'admin</p>
+    </div>
+</body>
+
+
+<?php include 'inc/footer.php';?>
